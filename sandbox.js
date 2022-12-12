60fb11b96a16fa728c6b29638c7de233cc4a45e0
Soundar Murugan <sm185711@ncr.com>
2022-12-12 14:35:51 +0530

    lesson 4

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,23 @@
-var character = "mario";
-var age = 30;
-var isBlackBelt = false;
-character = "luigi";
-var circumference = function (diameter) {
-    return diameter * Math.PI;
+// ARRAYS
+var names = ["luigi", "mario", "yoshi"];
+names.push("toad");
+names.push(3);
+names[0] = 3;
+var mixed = ["ken", 4, "chun-li", 8, 9, true];
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+// OBJECTS
+var ninja = {
+    name: "mario",
+    belt: "black",
+    age: 30
 };
-console.log(circumference(7.5));
+ninja.age = 40;
+ninja.name = "ryu";
+ninja.age = "30"; // same as variables
+ninja.skills = ["fighting", "sneaking"]; // cannot add properties
+ninja = {
+    name: "yoshi",
+    belt: "orange"
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,11 +1,34 @@
-let character = "mario";
-let age = 30;
-let isBlackBelt = false;
+// ARRAYS
+let names = ["luigi", "mario", "yoshi"];
 
-character = "luigi";
+names.push("toad");
 
-const circumference = (diameter: number) => {
-  return diameter * Math.PI;
+names.push(3);
+names[0] = 3;
+
+let mixed = ["ken", 4, "chun-li", 8, 9, true];
+
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+
+// OBJECTS
+let ninja = {
+  name: "mario",
+  belt: "black",
+  age: 30,
 };
 
-console.log(circumference(7.5));
+ninja.age = 40;
+ninja.name = "ryu";
+
+ninja.age = "30"; // same as variables
+
+ninja.skills = ["fighting", "sneaking"]; // cannot add properties
+
+ninja = {
+  name: "yoshi",
+  belt: "orange",
+  //   age: 40,
+  //   skills: []
+};
