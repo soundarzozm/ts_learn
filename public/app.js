cdb09a81e675eccbee778cffcd321eb9382a2051
Soundar Murugan <sm185711@ncr.com>
2022-12-18 12:47:31 +0530

    lesson 12

--- public/app.js
+++ public/app.js
@@ -1,13 +1,23 @@
 "use strict";
-// ! Tells the compiler that you are sure it isn't null
-const anchor = document.querySelector('a');
-// if (anchor) {
-// 	console.log(anchor.href)
-// }
-console.log(anchor.href);
-// Type is HTMLFormElement
-// const form = document.querySelector('form')
-// Type is Element
+// classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+console.log(invoices);
 const form = document.querySelector('.new-item-form');
 // console.log(form.children)
 // inputs
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,32 @@
-// ! Tells the compiler that you are sure it isn't null
-const anchor = document.querySelector('a')!
+// classes
+class Invoice {
+	client: string
+	details: string
+	amount: number
 
-// if (anchor) {
-// 	console.log(anchor.href)
-// }
+	constructor(c: string, d: string, a: number) {
+		this.client = c
+		this.details = d
+		this.amount = a
+	}
 
-console.log(anchor.href)
+	format() {
+		return `${this.client} owes $${this.amount} for ${this.details}`
+	}
+}
 
-// Type is HTMLFormElement
-// const form = document.querySelector('form')
+const invOne = new Invoice('mario', 'work on the mario website', 250)
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300)
 
-// Type is Element
+let invoices: Invoice[] = []
+invoices.push(invOne)
+invoices.push(invTwo)
+
+invOne.client = 'yoshi'
+invTwo.amount = 400
+
+console.log(invoices)
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement
 // console.log(form.children)
 
