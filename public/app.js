12ccd7455362548ded1822215b25c782eb678ae9
Soundar Murugan <sm185711@ncr.com>
2022-12-18 13:06:36 +0530

    lesson 14

--- public/app.js
+++ public/app.js
@@ -1,39 +1,11 @@
-"use strict";
-// classes
-class Invoice {
-    // Public is default
-    // readonly client: string
-    // private details: string
-    // public amount: number
-    // constructor(c: string, d: string, a: number) {
-    // 	this.client = c
-    // 	this.details = d
-    // 	this.amount = a
-    // }
-    // Quick way to do all this
-    constructor(
-    // Modifiers are compulsory for this
-    client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from './classes/invoice.js';
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-// Read-only so not changeable
-// invOne.client = 'yoshi'
 invTwo.amount = 400;
 invoices.forEach((inv) => {
     console.log(inv.format());
-    // Private so inaccessible
-    // console.log(inv.details)
-    // Readable
     console.log(inv.client);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,4 @@
-// classes
-class Invoice {
-	// Public is default
-	// readonly client: string
-	// private details: string
-	// public amount: number
-
-	// constructor(c: string, d: string, a: number) {
-	// 	this.client = c
-	// 	this.details = d
-	// 	this.amount = a
-	// }
-
-	// Quick way to do all this
-	constructor(
-		// Modifiers are compulsory for this
-		readonly client: string,
-		private details: string,
-		public amount: number
-	) {}
-
-	format() {
-		return `${this.client} owes $${this.amount} for ${this.details}`
-	}
-}
+import { Invoice } from './classes/invoice.js'
 
 const invOne = new Invoice('mario', 'work on the mario website', 250)
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300)
@@ -31,16 +7,9 @@ let invoices: Invoice[] = []
 invoices.push(invOne)
 invoices.push(invTwo)
 
-// Read-only so not changeable
-// invOne.client = 'yoshi'
 invTwo.amount = 400
 
 invoices.forEach((inv) => {
 	console.log(inv.format())
-
-	// Private so inaccessible
-	// console.log(inv.details)
-
-	// Readable
 	console.log(inv.client)
 })
