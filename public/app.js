17387edd4053a78c3c8f621680e19db2cf64c012
Soundar Murugan <sm185711@ncr.com>
2022-12-21 08:34:25 +0530

    lesson 15

--- public/app.js
+++ public/app.js
@@ -1,4 +1,16 @@
 import { Invoice } from './classes/invoice.js';
+const me = {
+    name: 'mario',
+    age: 20,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    },
+};
+console.log(me);
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices = [];
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,26 @@
 import { Invoice } from './classes/invoice.js'
 
+interface isPerson {
+	name: string
+	age: number
+	speak(a: string): void
+	spend(a: number): number
+}
+
+const me: isPerson = {
+	name: 'mario',
+	age: 20,
+	speak(text: string): void {
+		console.log(text)
+	},
+	spend(amount: number): number {
+		console.log('I spent', amount)
+		return amount
+	},
+}
+
+console.log(me)
+
 const invOne = new Invoice('mario', 'work on the mario website', 250)
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300)
 
